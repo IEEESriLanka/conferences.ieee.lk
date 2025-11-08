086de1384163b48b529849e6ebf63b3368c05267
Dhammika Marasinghe <dhammikamare@gmail.com>
2025-11-08 17:21:57 +0530

    Setup deployment

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,18 @@ export default defineConfig({
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: 'public/index.html',
+        conferences: 'public/conferences.html',
+        committee: 'public/committee.html',
+        guidelines: 'public/guidelines.html',
+        resources: 'public/resources.html',
+        workshops: 'public/workshops.html',
+        conf: 'public/conf/details.html',
+        404: 'public/404.html'
+      }
+    }
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
